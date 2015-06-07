1d0a8b5e53e25e575183c0c9e5c307343f0968e6
Jakub Foltyn <foltyn.jakub@outlook.com>
2015-06-07 18:37:28 +0200

    Added section 2

--- kreator-stylu/js/app.js
+++ kreator-stylu/js/app.js
@@ -1,3 +1,30 @@
-
-
-
+angular.module('kreatorStylu',[])
+.controller('OfferPanelCtrl', function(){
+    this.offers = [
+        {
+            image: './resources/images/oferta_image_1.png',
+            title: 'Strzyżenie',
+            description: 'strzyżenie damskie od 40 PLN\nstrzyżenie męskie od 30 PLN\nstrzyżenie dzecięce od 30 PLN'
+        },
+        {
+            image: './resources/images/oferta_image_2.png',
+            title: 'Koloryzacja',
+            description: 'koloryzacja od 100 PLN'
+        },
+        {
+            image: './resources/images/oferta_image_3.png',
+            title: 'Stylizacja',
+            description: 'Cennik standardowy:\nupięcie ślubne od 90 PLN\nupięcie okolicznościowe od 80 PLN'
+        },
+        {
+            image: './resources/images/oferta_image_4.png',
+            title: 'Zabiegi',
+            description: 'PRO-KERATIN REFIL 60 PLN\n POWER REPAIR Lipidum 60 PLN\n PRIMER REPAIR Lipidum 90 PLN\n LISSCEUTIC 90 PLN'
+        },
+        {
+            image: './resources/images/oferta_image_3.png',
+            title: 'Stylizacja',
+            description: 'Cennik standardowy:\nupięcie ślubne od 90 PLN\nupięcie okolicznościowe od 80 PLN'
+        },
+    ]
+})
