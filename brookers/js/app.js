fe5588bfe6f281fc87375b2ab97b50d9a43d4367
Jakub Foltyn <foltyn.jakub@outlook.com>
2015-06-24 22:47:48 +0200

    moved slider content do app.js array, and set ProductSliderController

--- brookers/js/app.js
+++ brookers/js/app.js
@@ -1,5 +1,28 @@
 angular.module('brookers',[])
-.controller('ProductsPanelCtrl', function(){
+.controller('ProductsSliderCtrl', function(){
+    this.products = [
+        {
+            image: './resources/images/Produkty/produkty_product_1.png',
+            name: 'Galaxy Note 4',
+            price: '2125'
+        },
+        {
+            image: './resources/images/Produkty/produkty_product_2.png',
+            name: 'Galaxy S 6',
+            price: '2279'
+        },
+        {
+            image: './resources/images/Produkty/produkty_product_3.png',
+            name: 'Galaxy S5',
+            price: '1490'
+        },
+        {
+            image: './resources/images/Produkty/produkty_product_2.png',
+            name: 'Galaxy S6',
+            price: '2279'
+        }
+    ]
+}).controller('ProductsPanelCtrl', function(){
     this.offers = [
         {
             image: '../resources/images/Produkty/produkty_product_1_small.png',
@@ -25,7 +48,7 @@ angular.module('brookers',[])
             image: '../resources/images/Produkty/produkty_product_5_small.png',
             name: 'Galaxy S4',
             price: '1490'
-        }
+        },
         {
             image: '../resources/images/Produkty/produkty_product_6_small.png',
             name: 'Galaxy S6',
